ca92a69679126f42a4c7eec97791b27713a76799
Alejandro Sánchez <23413433+alejos4n@users.noreply.github.com>
2022-06-02 21:48:30 -0500

    muestra salida
    muestra la salida de la ejecución en la creación de un compartment

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+# Outputs for compartment
+
+output "compartment_desde_terraform" {
+  value = oci_identity_compartment.tf-compartment.name
+}
+
+output "compartment-OCID" {
+  value = oci_identity_compartment.tf-compartment.id
+}
