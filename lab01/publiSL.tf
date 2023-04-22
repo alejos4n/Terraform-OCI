fa107711d7ef78f1a09685023fb4dfe91cb42ae5
Alejandro Sánchez <23413433+alejos4n@users.noreply.github.com>
2023-04-22 02:48:06 -0500

    actualziacion repos
    Archivos que fueron actualizados, crean una vcn y una subnet public

---
+++ lab01/publiSL.tf
@@ -0,0 +1,11 @@
+# Source from https://registry.terraform.io/providers/oracle/oci/latest/docs/resources/core_security_list
+
+resource "oci_core_security_list" "public-security-list"{
+
+# Required
+  compartment_id = var.compartment_ocid
+  vcn_id = module.vcn.vcn_id
+
+# Optional
+  display_name = "security-list-for-public-subnet"
+}
