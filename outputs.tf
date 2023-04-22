525a8330877923d5e9a427bcf3dbb853fba1f36a
Alejandro Sánchez <23413433+alejos4n@users.noreply.github.com>
2023-04-22 02:43:57 -0500

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-# Outputs for compartment
-
-output "compartment_desde_terraform" {
-  value = oci_identity_compartment.tf-compartment.name
-}
-
-output "compartment-OCID" {
-  value = oci_identity_compartment.tf-compartment.id
-}
