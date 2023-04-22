fb46b1754d8740b48e5891d45be9a519f6ce6c5b
Alejandro Sánchez <23413433+alejos4n@users.noreply.github.com>
2023-04-22 02:43:25 -0500

    Delete compartment.tf

--- compartment.tf
+++
@@ -1,6 +0,0 @@
-resource "oci_identity_compartment" "tf-compartment" {
-  # Required
-  compartment_id = var.comparment_ocid
-  description    = "Compartment for Terraform resources."
-  name           = "compartment_desde_terraform"
-}
